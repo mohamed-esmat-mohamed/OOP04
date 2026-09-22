e788f16a3271eb4a074c4f330b88d264af4f88bc
Ecc <Ecc@DESKTOP-E983V9F>
2026-09-23 02:28:41 +0300

    ITrackable

---
+++ Smart-Delivery/ITrackable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Smart_Delivery
+{
+    //ITrackable
+    public interface ITrackable
+    {
+        string GetTrackingStatus();
+    }
+}
