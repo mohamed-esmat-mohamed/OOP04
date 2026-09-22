2e38e28a559b8f8bfbe159d0c22492dc4c75c8f4
Ecc <Ecc@DESKTOP-E983V9F>
2026-09-23 02:32:21 +0300

    DeliveryReport

---
+++ Smart-Delivery/DeliveryReport.cs
@@ -0,0 +1,25 @@
+﻿using Smart_Delivery;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Smart_Delivery
+
+{
+    // DeliveryReport
+    public static class DeliveryReport
+    {
+        public static void PrintShipment(ITrackable shipment)
+        {
+            Console.WriteLine(shipment.GetTrackingStatus());
+        }
+
+        public static void PrintInsurance(IInsurable shipment)
+        {
+            string typeName = shipment.GetType().Name;
+            string label = typeName.Substring(0, typeName.Length - "Shipment".Length) + " Shipment";
+
+            Console.WriteLine(label + " Insurance : " + shipment.CalculateInsurance().ToString("0.00") + " EGP");
+        }
+    }
+}
