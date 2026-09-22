015ed41e79079eb6f9fb03c6ad9cfdf953e689cf
Ecc <Ecc@DESKTOP-E983V9F>
2026-09-23 02:30:01 +0300

    IInsurable

---
+++ Smart-Delivery/IInsurable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Smart_Delivery
+{
+    //IInsurable
+    internal interface IInsurable
+    {
+        decimal CalculateInsurance();
+    }
+}
