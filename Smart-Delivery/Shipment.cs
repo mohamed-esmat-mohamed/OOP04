87009deb488859a9e99ff9f1333c7d5d90832770
Ecc <Ecc@DESKTOP-E983V9F>
2026-09-23 02:16:42 +0300

    Convert Shipment

---
+++ Smart-Delivery/Shipment.cs
@@ -0,0 +1,39 @@
+﻿
+
+using System;
+
+namespace Smart_Delivery
+{
+    // Convert Shipment
+    public abstract class Shipment
+    {
+        public string TrackingCode { get; set; }
+        public string Description { get; set; }
+        public decimal Weight { get; set; }
+        public decimal DeliveryFee { get; set; }
+        public DeliveryAddress Destination { get; set; }
+
+        public Shipment(string trackingCode, string description, decimal weight, decimal deliveryFee, DeliveryAddress destination)
+        {
+            
+            if (string.IsNullOrWhiteSpace(trackingCode))
+                throw new ArgumentException("Tracking code cannot be empty.");
+
+            if (weight <= 0)
+                throw new ArgumentException("Weight must be greater than zero.");
+
+            if (deliveryFee < 0)
+                throw new ArgumentException("Delivery fee cannot be negative.");
+
+            TrackingCode = trackingCode;
+            Description = description;
+            Weight = weight;
+            DeliveryFee = deliveryFee;
+            Destination = destination;
+        }
+
+         //Create Abstract 
+        public abstract decimal EstimatedCost { get; }
+        public abstract void PrintShipment();
+    }
+}
