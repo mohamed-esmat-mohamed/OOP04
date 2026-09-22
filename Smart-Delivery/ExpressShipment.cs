33a801b6a4d9ff7acfac64279cbd60b4b7950d69
Ecc <Ecc@DESKTOP-E983V9F>
2026-09-23 02:25:10 +0300

    Updata8%

---
+++ Smart-Delivery/ExpressShipment.cs
@@ -0,0 +1,42 @@
+﻿using Smart_Delivery;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Smart_Delivery
+
+{
+    // Update 8%
+    public class ExpressShipment : Shipment, ITrackable, IInsurable
+    {
+        public decimal ExtraFee { get; set; }
+
+        public ExpressShipment(string trackingCode, string description, decimal weight, decimal deliveryFee, decimal extraFee, DeliveryAddress destination)
+            : base(trackingCode, description, weight, deliveryFee, destination)
+        {
+            ExtraFee = extraFee;
+        }
+
+        public override decimal EstimatedCost
+        {
+            get { return DeliveryFee + ExtraFee; }
+        }
+
+        public override void PrintShipment()
+        {
+            Console.WriteLine("Express Shipment");
+            Console.WriteLine();
+            Console.WriteLine("Tracking Code : " + TrackingCode);
+            Console.WriteLine("Extra Fee     : " + ExtraFee + " EGP");
+            Console.WriteLine("Estimated Cost: " + EstimatedCost + " EGP");
+        }
+        public string GetTrackingStatus()
+        {
+            return "Shipment " + TrackingCode + " is Out for Delivery.";
+        }
+        public decimal CalculateInsurance()
+        {
+            return EstimatedCost * 0.08m;
+        }
+    }
+}
