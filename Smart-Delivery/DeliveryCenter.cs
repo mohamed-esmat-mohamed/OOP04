d2183b4835c7cac0ec2d006374d425f2ce46e36b
Ecc <Ecc@DESKTOP-E983V9F>
2026-09-23 02:34:57 +0300

    DeliveryCenter

---
+++ Smart-Delivery/DeliveryCenter.cs
@@ -0,0 +1,38 @@
+﻿using Smart_Delivery;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Smart_Delivery
+
+{
+    public class DeliveryCenter
+    {
+        private List<Shipment> items = new List<Shipment>();
+
+        public void AddShipment(Shipment shipment)
+        {
+            items.Add(shipment);
+        }
+
+        public void PrintAllShipments()
+        {
+            foreach (Shipment s in items)
+            {
+                s.PrintShipment();
+                Console.WriteLine();
+                Console.WriteLine("--------------------------------------------");
+                Console.WriteLine();
+            }
+        }
+
+        public void PrintTrackingStatuses()
+        {
+            foreach (ITrackable t in items)
+            {
+                Console.WriteLine(t.GetTrackingStatus());
+                Console.WriteLine();
+            }
+        }
+    }
+}
