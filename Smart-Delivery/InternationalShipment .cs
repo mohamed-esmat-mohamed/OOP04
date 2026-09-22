3c462c83dbf7cc27cabbb555120206de82e64b63
Ecc <Ecc@DESKTOP-E983V9F>
2026-09-23 02:27:17 +0300

    Update 12%

---
+++ Smart-Delivery/InternationalShipment .cs
@@ -0,0 +1,43 @@
+﻿using Smart_Delivery;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Smart_Delivery
+
+{
+    //Update 12%
+    public class InternationalShipment : Shipment, ITrackable, IInsurable
+    {
+        public string DestinationCountry { get; set; }
+
+        public InternationalShipment(string trackingCode, string description, decimal weight, decimal deliveryFee, string destinationCountry, DeliveryAddress destination)
+            : base(trackingCode, description, weight, deliveryFee, destination)
+        {
+            DestinationCountry = destinationCountry;
+        }
+
+        public override decimal EstimatedCost
+        {
+            get { return DeliveryFee; }
+        }
+
+        public override void PrintShipment()
+        {
+            Console.WriteLine("International Shipment");
+            Console.WriteLine();
+            Console.WriteLine("Tracking Code       : " + TrackingCode);
+            Console.WriteLine("Destination Country : " + DestinationCountry);
+            Console.WriteLine("Estimated Cost      : " + EstimatedCost + " EGP");
+        }
+
+        public string GetTrackingStatus()
+        {
+            return "Shipment " + TrackingCode + " has been Delivered.";
+        }
+        public decimal CalculateInsurance()
+        {
+            return EstimatedCost * 0.12m;
+        }
+    }
+}
