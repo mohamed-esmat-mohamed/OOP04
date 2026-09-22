839ee4fe25da1eb8588249d62944f181544c42a5
Ecc <Ecc@DESKTOP-E983V9F>
2026-09-23 02:36:10 +0300

    DeliveryAddress

---
+++ Smart-Delivery/DeliveryAddress.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Smart_Delivery
+
+{
+    //  
+    public class DeliveryAddress
+    {
+        public string City { get; set; }
+        public string Country { get; set; }
+
+        public DeliveryAddress(string city, string country)
+        {
+            City = city;
+            Country = country;
+        }
+
+        public override string ToString()
+        {
+            return City + ", " + Country;
+        }
+    }
+}
